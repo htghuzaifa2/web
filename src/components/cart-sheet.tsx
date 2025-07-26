dce7f3c4f67c77395aa0f2120a91aac050f0d7d3
HTG <namej5375@gmail.com>
2025-07-26 11:36:07 +0000

    now when quantity 1 then i click on less its remove the item from cart m

--- src/components/cart-sheet.tsx
+++ src/components/cart-sheet.tsx
@@ -59,6 +59,7 @@ export function CartSheet() {
                                   size="icon"
                                   className="h-8 w-8"
                                   onClick={() => updateQuantity(item.id, item.quantity - 1)}
+                                  disabled={item.quantity <= 1}
                                 >
                                   <Minus className="h-4 w-4" />
                                 </Button>
