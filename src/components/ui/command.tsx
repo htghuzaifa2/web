13c0c969f6a64d801c399368effdfef8b7e445f5
HTG <namej5375@gmail.com>
2025-07-31 21:16:27 +0000

    hey search again not working fix the issues make sure ots responsive acr

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -24,17 +24,12 @@ const Command = React.forwardRef<
 ))
 Command.displayName = CommandPrimitive.displayName
 
-interface CommandDialogProps extends DialogProps {
-    children?: React.ReactNode;
-}
+interface CommandDialogProps extends DialogProps {}
 
-const CommandDialog = React.forwardRef<
-  React.ElementRef<typeof DialogContent>,
-  CommandDialogProps
->(({ children, ...props }, ref) => {
+const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent ref={ref} className="overflow-hidden p-0 shadow-lg">
+      <DialogContent className="overflow-hidden p-0 shadow-lg">
         <Command 
           className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           <DialogTitle className="sr-only">Search</DialogTitle>
@@ -43,8 +38,7 @@ const CommandDialog = React.forwardRef<
       </DialogContent>
     </Dialog>
   )
-})
-CommandDialog.displayName = "CommandDialog"
+}
 
 
 const CommandInput = React.forwardRef<
