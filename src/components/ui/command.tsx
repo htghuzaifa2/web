e2db3d9d9360a88e9ebb27625e36760502b241ed
HTG <namej5375@gmail.com>
2025-07-31 21:11:14 +0000

    fix all errors.

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -28,11 +28,13 @@ interface CommandDialogProps extends DialogProps {
     children?: React.ReactNode;
 }
 
-const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
-  const { onOpenChange, ...dialogProps } = props;
+const CommandDialog = React.forwardRef<
+  React.ElementRef<typeof DialogContent>,
+  CommandDialogProps
+>(({ children, ...props }, ref) => {
   return (
-    <Dialog onOpenChange={onOpenChange}>
-      <DialogContent {...dialogProps} className="overflow-hidden p-0 shadow-lg">
+    <Dialog {...props}>
+      <DialogContent ref={ref} className="overflow-hidden p-0 shadow-lg">
         <Command 
           className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           <DialogTitle className="sr-only">Search</DialogTitle>
@@ -41,7 +43,9 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
       </DialogContent>
     </Dialog>
   )
-}
+})
+CommandDialog.displayName = "CommandDialog"
+
 
 const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
