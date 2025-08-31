76f2778cab96d81502e20b2ff45fb34326680542
HTG <namej5375@gmail.com>
2025-08-31 23:32:14 +0000

    no not work on both modes only work on light mode also it uses so di lig

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -50,7 +50,7 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <div className="mt-8 border-t pt-4 text-center text-sm font-bold animate-copyright-text">
+        <div className="mt-8 border-t pt-4 text-center text-sm font-bold animate-rgb-text">
           &copy; {new Date().getFullYear()} huzi.pk. All Rights Reserved.
         </div>
       </div>
