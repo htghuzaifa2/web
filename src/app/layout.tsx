e979a4ed5436c40877f4f523da5d9f7396f1f3ac
HTG <namej5375@gmail.com>
2025-09-01 00:28:56 +0000

    now once finnal enhancements because i am so woried about performance on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -103,6 +103,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://i.postimg.cc" />
         <link rel="dns-prefetch" href="https://i.postimg.cc" />
+        <link rel="preload" href="/logo.webp" as="image" />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
