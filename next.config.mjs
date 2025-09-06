7b919e55ac69190c31e0d1c09071c402e0052e5c
HTG <namej5375@gmail.com>
2025-09-06 22:31:22 +0000

    now are you sure my all errors will be fixed and it will deployed withou

--- next.config.mjs
+++ next.config.mjs
@@ -9,12 +9,6 @@ const nextConfig = {
                 port: '',
                 pathname: '/**',
             },
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-                port: '',
-                pathname: '/**',
-            }
         ],
     },
 };
