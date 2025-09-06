7a8f1a1f7d08be61d80465ccf77e72b0d3e86f3b
HTG <namej5375@gmail.com>
2025-09-06 22:07:00 +0000

    2025-09-06T22:03:47.529045Z	Cloning repository... 2025-09-06T22:03:48.15

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'i.postimg.cc',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
