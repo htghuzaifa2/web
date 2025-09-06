687019ff0871fa4532792b94b6a58b6cbc33ea82
HTG <namej5375@gmail.com>
2025-09-06 22:23:15 +0000

    2025-09-06T22:18:56.611932Z	Cloning repository... 2025-09-06T22:18:57.25

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,16 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'i.postimg.cc',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-                port: '',
-                pathname: '/**',
-            }
-        ],
-    },
-     // Required for Genkit to work with Next.js App Router
-    // See: https://firebase.google.com/docs/genkit/nextjs-app-router#add_a_next.config.js_file
-    experimental: {
-        instrumentationHook: true,
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
