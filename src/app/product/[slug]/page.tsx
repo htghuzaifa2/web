d46a07f8d69fb86b563b1fb29ce4efeb45ca089e
HTG <namej5375@gmail.com>
2025-10-07 17:18:47 +0000

    Try fixing this error: `Console Error: [ Server ] Error: In route /produ

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -62,6 +62,7 @@ export async function generateMetadata({ params }: ProductPageProps): Promise<Me
   };
 }
 
-export default function ProductPage({ params: { slug } }: ProductPageProps) {
+export default function ProductPage({ params }: ProductPageProps) {
+  const { slug } = params;
   return <ProductDetailsWrapper slug={slug} />;
 }
