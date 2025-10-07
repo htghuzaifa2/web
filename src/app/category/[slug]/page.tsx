c9b1f43a4d64cc2393f0f1797f4a275f1776c9e5
HTG <namej5375@gmail.com>
2025-10-07 17:46:17 +0000

    Try fixing this error: `Console Error: [ Server ] Error: In route /produ

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -47,6 +47,6 @@ export async function generateMetadata({ params }: CategoryPageProps): Promise<M
   };
 }
 
-export default function CategoryPage({ params: { slug } }: CategoryPageProps) {
+export default async function CategoryPage({ params: { slug } }: CategoryPageProps) {
   return <CategoryWrapper slug={slug} />;
 }
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -61,6 +61,6 @@ export async function generateMetadata({ params }: ProductPageProps): Promise<Me
   };
 }
 
-export default function ProductPage({ params: { slug } }: ProductPageProps) {
+export default async function ProductPage({ params: { slug } }: ProductPageProps) {
   return <ProductDetailsWrapper slug={slug} />;
 }
