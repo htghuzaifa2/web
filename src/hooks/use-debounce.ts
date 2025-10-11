950b2bbbd84774034fc239b2fa485f33b371bf92
HTG <namej5375@gmail.com>
2025-10-11 16:48:55 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,29 @@
+
+"use client";
+
+import { useState, useEffect } from 'react';
+
+// A hook for debouncing value changes
+export function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    // Set debouncedValue to value (passed in) after the specified delay
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    // Return a cleanup function that will be called every time ...
+    // ... useEffect is re-called. useEffect will only be re-called ...
+    // ... if value or delay changes (see the inputs array below). 
+    // This is how we prevent debouncedValue from changing if value is ...
+    // ... changing rapidly.
+    return () => {
+      clearTimeout(handler);
+    };
+  },
+  // Only re-call effect if value or delay changes
+  [value, delay]);
+
+  return debouncedValue;
+}
