9900bec07f0acf11c4b6aacb8a86d5a62cc11a32
HTG <namej5375@gmail.com>
2025-11-13 00:32:41 +0000

    INPUT FILE (wrong format): Root element: <rss version="2.0" xmlns:g="htt

--- src/app/products.xml/route.ts
+++ src/app/products.xml/route.ts
@@ -4,35 +4,27 @@ import type { Product } from "@/lib/types";
 
 export async function GET() {
   const products: Product[] = productsData;
+  const siteUrl = "https://huzi.pk";
+  const today = new Date().toISOString().split('T')[0];
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:g="http://base.google.com/ns/1.0">
-<channel>
-<title>huzi.pk Product Feed</title>
-<link>https://huzi.pk</link>
-<description>Product feed for huzi.pk</description>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${products
   .map(
     (p) => `
-  <item>
-    <g:id>${p.id}</g:id>
-    <g:title>${p.name.replace(/&/g, "&amp;")}</g:title>
-    <g:link>https://huzi.pk/product/${p.slug}</g:link>
-    <g:price>${p.price.toFixed(2)} PKR</g:price>
-    <g:image_link>${p.image}</g:image_link>
-    <g:availability>${p.stock === undefined || p.stock > 0 ? "in stock" : "out of stock"}</g:availability>
-    <g:description>${p.description.replace(/&/g, "&amp;")}</g:description>
-    <g:brand>huzi.pk</g:brand>
-    <g:condition>new</g:condition>
-    ${p.additionalImages?.map((img) => `<g:additional_image_link>${img}</g:additional_image_link>`).join("") || ""}
-  </item>`
+  <url>
+    <loc>${siteUrl}/product/${p.slug}</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`
   )
   .join("")}
-</channel>
-</rss>`;
+</urlset>`;
 
   return new Response(xml, {
     headers: {
-      "Content-Type": "application/rss+xml",
+      "Content-Type": "application/xml",
     },
   });
 }
